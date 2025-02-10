7e2179b88d7b54ba8f28539fe2d3a4988f128492
Mathys <mathys.calero@gmail.com>
2025-02-10 14:43:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Attendre que le DOM soit complètement chargé avant d'exécuter le script
 document.addEventListener('DOMContentLoaded', () => {
     // Sélectionner le bouton de changement de thème par son ID
-    const themeToggle = document.getElementById('theme-toggle');
+    const themeChangement = document.getElementById('theme-changement');
     
     // Ajouter un écouteur d'événement pour le clic sur le bouton
-    themeToggle.addEventListener('click', () => {
+    themeChangement.addEventListener('click', () => {
         // Basculer la classe 'dark-theme' sur l'élément body
         // Cela permet de changer le thème de la page
         document.body.classList.toggle('dark-theme');
